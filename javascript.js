f91c1623e6e0a90584ecc7354587c7a2856f6d9e
Quincy <quincythai50@icloud.com>
2023-06-25 03:14:16 -0700

    Complete skeleton JS

--- javascript.js
+++ javascript.js
@@ -19,6 +19,63 @@ function getComputerChoice() {
   return choice;
 }
 
+let rounds = 0;
+let playerWins = 0;
+let computerWins = 0;
+
 function playRound(playerChoice, computerChoice) {
-  
+  let string;
+
+  if (playerChoice === computerChoice) {
+    string = `You tied. ${playerChoice} ties ${computerChoice}.`;
+  } else if (playerChoice === "Bears" && computerChoice === "Honey" || 
+  playerChoice === "Bees" && computerChoice === "Bears" || 
+  playerChoice === "Honey" && computerChoice === "Bees") {
+    playerWins++;
+    string = `You win! ${playerChoice} beats ${computerChoice}!`;
+  } else {
+    computerWins++;
+    string = `You lost.. ${playerChoice} lose to ${computerChoice}.`;
+  }
+
+  rounds++;
+  updateScores(string);
 }
+
+const bearsButton = document.querySelector('#bear-button');
+const beesButton = document.querySelector('#bees-button');
+const honeyButton = document.querySelector('#honey-button');
+const outcomeParagraph = document.querySelector('#outcome-paragraph');
+
+function game() {
+  bearsButton.addEventListener('click', () => {
+    handleButtonClick("Bears");
+  });
+  beesButton.addEventListener('click', () => {
+    handleButtonClick("Bees");
+  });
+  honeyButton.addEventListener('click', () => {
+    handleButtonClick("Honey");
+  });
+}
+
+function handleButtonClick(choice) {
+  playRound(choice, getComputerChoice());
+}
+
+const roundsParagraph = document.querySelector('#round-score');
+const myScoreParagraph = document.querySelector('#my-score');
+const cpuScoreParagraph = document.querySelector('#cpu-score');
+
+function updateScores(string) {
+  roundsParagraph.textContent = `Rounds: ${rounds}`;
+  myScoreParagraph.textContent = `You: ${playerWins}`;
+  cpuScoreParagraph.textContent = `Oski: ${computerWins}`;
+  outcomeParagraph.textContent = string;
+}
+
+function endGame(winner) {
+  outcomeParagraph.textContent = `The winner is: ${winner}`;
+}
+
+game();
